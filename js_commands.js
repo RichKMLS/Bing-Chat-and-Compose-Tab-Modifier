b87589ae0df7fedcb9353e9d213292b44bf9f81b
Rich K. MLS <105183376+RichKMLS@users.noreply.github.com>
2023-05-20 22:01:10 +0200

    Update js_commands.js

--- js_commands.js
+++ js_commands.js
@@ -1,6 +1,6 @@
 // the following can be ran as a snippet in the developer console manually.
 
-// Define an IIFE function that sets the maxLength property of the search box to 25000 if the website is example1.com.
+// Define an IIFE function that sets the maxLength property of the search box to 25000 if on chat.
 (function() {
     if (window.location.hostname === 'https://edgeservices.bing.com/edgesvc/chat') {
         let root = document.querySelector("#b_sydConvCont > cib-serp")
@@ -12,7 +12,7 @@
 
 // Define an IIFE function that removes elements with specified selectors from the DOM,
 // changes the text content of a button to ‘Prompt’, and sets the maxLength property
-// and placeholder text of a text input element if the website is example2.com.
+// and placeholder text of a text input element if on compose.
 (function() {
     if (window.location.hostname === 'https://edgeservices.bing.com/edgesvc/compose') {
         let decorations = ['.option-section', '#input_heading',
